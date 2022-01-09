46cce1ca156aa39b70977f8a50a03d56cd37f5c6
rogermiranda1000 <rogermirandaperez@hotmail.com>
2022-01-09 16:15:07 +0100

    Fix: removed commented line

--- cgi-bin/index.sh
+++ cgi-bin/index.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 token=`echo "$HTTP_COOKIE" | grep -P -o '(?<=token=)[^;]+'` # token de login (si en té)
-if [ -z "$token" ]; then
-	./login.sh
-else
-	./login.sh "$token"
-fi
+./login.sh "$token"
 ret=$?
 if [ $ret -ne 0 ]; then
 	exit 0 # l'usuari ha de fer login; login.sh ja carrega la pàgina
@@ -14,4 +10,3 @@ fi
 echo "content-type: text/html; charset=utf-8"
 echo
 cat "src/index.html"
-#echo "" | su -l rogermiranda1000 -c 'whoami'
