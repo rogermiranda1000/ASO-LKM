628f814495d9831e0f0ce9dd7af895ab4f492c99
rogermiranda1000 <rogermirandaperez@hotmail.com>
2022-01-09 17:16:39 +0100

    Fix: login invalid token

--- cgi-bin/index.sh
+++ cgi-bin/index.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 token=`echo "$HTTP_COOKIE" | grep -P -o '(?<=token=)[^;]+'` # token de login (si en té)
-./login.sh "$token"
-ret=$?
+if [ -z "$token" ]; then
+	./login.sh; ret=$?
+else
+	./login.sh "$token"; ret=$?
+fi
+
 if [ $ret -ne 0 ]; then
 	exit 0 # l'usuari ha de fer login; login.sh ja carrega la pàgina
 fi
